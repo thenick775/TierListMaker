f4471ab892502fc051389b922518f04debb0e2da
Nick VanCise <nickvancise@MacBook-Pro.local>
2023-05-26 23:38:24 -0700

    fix: row header color change
    - remapping rows to numeric names messed up class removal

--- assets/js/saveModal.js
+++ assets/js/saveModal.js
@@ -4,13 +4,15 @@ function saveModal() {
   );
   const data = modal.data;
   let tier_row = document.getElementById(data.tier);
+  const tier_row_num = parseInt(data.tier.replace(/^tier-/, ""));
+  const tier_row_name = tiers[tier_row_num % tiers.length].toLowerCase();
 
   const stretch = document.getElementById("flexSwitchCheckDefault").checked;
   const color = document.getElementById("colorInput").value;
   const files = toArray(document.getElementById("formFile").files);
 
   tier_row.firstChild.classList.remove(
-    `tlm-bgcolor-${data.tier.replace(/^tier-/, "")}`
+    `tlm-bgcolor-${tier_row_name}`
   );
   tier_row.firstChild.style.backgroundColor = color;
 
