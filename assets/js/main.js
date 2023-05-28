aef8c5c73f125d521cf104d97fc22e5fa746b904
Nick VanCise <nickvancise@MacBook-Pro.local>
2023-05-27 21:11:45 -0700

    feat: dragging outside of tier rows removes image
    - add dragging outside of tier rows removes image
    
    - also fixes issue with images making their way into the table row header elems

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,25 @@ document.onreadystatechange = () => {
     .getElementById("addContentModalSaveBtn")
     .addEventListener("click", saveModal);
   document.getElementById("addRowButton").addEventListener("click", addRow);
+
+  // dragging elements outside of table rows will bubble up to the body handler
+  // this drop handler will remove the dragged image
+  const body = document.body;
+
+  ["enter", "over", "leave"].forEach((name) => {
+    body.addEventListener(`drag${name}`, preventDefault, false);
+  });
+
+  body.addEventListener(
+    "drop",
+    (ev) => {
+      preventDefault(ev);
+
+      const id = ev.dataTransfer.getData("text");
+      const el = document.getElementById(id);
+
+      el.parentNode.removeChild(el);
+    },
+    false
+  );
 };
